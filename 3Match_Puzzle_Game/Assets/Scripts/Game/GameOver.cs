55be1301e50ec91abe2eca945ee0e5d8b1990112
namsuhan0322 <rnatpakfl@gmail.com>
2024-05-11 20:22:10 +0900

    [Feat] 시간 Bar 구현, GameOver 구현

---
+++ 3Match_Puzzle_Game/Assets/Scripts/Game/GameOver.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]  GameObject GameOverText;
+    [SerializeField]  float maxTime = 120f;
+    float timerLeft;
+    Image timerBar;
+
+    void Start()
+    {
+        GameOverText.SetActive(false);
+        timerBar = GetComponent<Image>();
+        timerLeft = maxTime;
+    }
+
+    void Update()
+    {
+        if (timerLeft > 0)
+        {
+            timerLeft -= Time.deltaTime;
+            timerBar.fillAmount = timerLeft / maxTime;
+        }
+        else
+        {
+            GameOverText.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
