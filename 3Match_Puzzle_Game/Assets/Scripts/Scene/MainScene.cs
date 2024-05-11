b428c2d3685806d2a32b4489b5ee4e8738add234
namsuhan0322 <rnatpakfl@gmail.com>
2024-05-11 15:57:23 +0900

    [Feat] 로딩바 수정, 스테이지 선택 구현, 백버튼 구현

--- 3Match_Puzzle_Game/Assets/Scripts/Scene/MainScene.cs
+++ 3Match_Puzzle_Game/Assets/Scripts/Scene/MainScene.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScene : MonoBehaviour
 {
-    public void GoToGame()
+    public void SelectionStage()
+    {
+        SceneManager.LoadScene("SelectionStage");
+    }
+    
+    public void GoToStage1()
     {
         LoadingBarController.LoadScene("Stage1");                  // 게임 씬으로 돌아간다.
+    }
+    
+    public void GoToStage2()
+    {
+        LoadingBarController.LoadScene("Stage2");                  // 게임 씬으로 돌아간다.
+    }
+    
+    public void GoToStage3()
+    {
+        LoadingBarController.LoadScene("Stage3");                  // 게임 씬으로 돌아간다.
     }
 }
