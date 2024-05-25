6b604f92d02ed7d9a75ff0a79fb8a93ee89f53ee
tnqkqkr <psn050675@gmail.com>
2024-05-25 17:13:40 +0900

    [Feat] 플레이어 및 Enemy 체력바 구현(다시올림)

--- 3Match_Puzzle_Game/Assets/Scripts/Enemy/EnemyAttack.cs
+++ 3Match_Puzzle_Game/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -8,7 +8,7 @@ public class EnemyAttack : MonoBehaviour
     public float attackRange = 1.0f; // 공격 범위입니다.
     public float attackRate = 1.0f; // 초당 공격 횟수입니다.
     private float nextAttackTime = 0f; // 다음 공격이 가능한 시간을 추적합니다.
-    public Transform player; // 플레이어의 Transform을 참조합니다.
+    public Transform Player; // 플레이어의 Transform을 참조합니다.
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +19,12 @@ public class EnemyAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player == null)
+        if (Player == null)
         {
             return; // 플레이어가 없으면 업데이트를 종료합니다.
         }
 
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position); // 플레이어와의 거리를 계산합니다.
+        float distanceToPlayer = Vector2.Distance(transform.position, Player.position); // 플레이어와의 거리를 계산합니다.
         if (distanceToPlayer <= attackRange && Time.time >= nextAttackTime)
         {
             Attack(); // 플레이어가 공격 범위 내에 있고 공격이 가능한 시간이 되면 공격합니다.
@@ -34,7 +34,7 @@ public class EnemyAttack : MonoBehaviour
 
     void Attack()
     {
-        player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+        Player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
     }
 
     void OnDrawGizmosSelected()
