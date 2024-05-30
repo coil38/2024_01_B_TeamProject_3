9f2c4cde6f5f5c6d8fbc9d650374988862951c67
tnqkqkr <psn050675@gmail.com>
2024-05-30 21:46:34 +0900

    [Feat] Enemy 이동 및 데미지 구현(아직 완벽하지않음)

--- 3Match_Puzzle_Game/Assets/Scripts/Enemy/EnemyAttack.cs
+++ 3Match_Puzzle_Game/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,35 +4,17 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public int attackDamage = 10; // 공격 시 가할 데미지입니다.
-    public float attackRange = 1.0f; // 공격 범위입니다.
-    public float attackRate = 1.0f; // 초당 공격 횟수입니다.
-    private float nextAttackTime = 0f; // 다음 공격이 가능한 시간을 추적합니다.
-    public Transform Player; // 플레이어의 Transform을 참조합니다.
+   public int damage = 10;
 
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Player == null)
+        if(collision.gameObject.CompareTag("Player"))
         {
-            return; // 플레이어가 없으면 업데이트를 종료합니다.
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
         }
-
-        float distanceToPlayer = Vector2.Distance(transform.position, Player.position); // 플레이어와의 거리를 계산합니다.
-        if (distanceToPlayer <= attackRange && Time.time >= nextAttackTime)
-        {
-            Attack(); // 플레이어가 공격 범위 내에 있고 공격이 가능한 시간이 되면 공격합니다.
-            nextAttackTime = Time.time + 1f / attackRate; // 다음 공격 시간을 설정합니다.
-        }
-    }
-
-    void Attack()
-    {
-        Player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red; // 기즈모 색상을 빨간색으로 설정합니다.
-        Gizmos.DrawWireSphere(transform.position, attackRange); // 공격 범위를 나타내는 구를 그립니다.
     }
 }
