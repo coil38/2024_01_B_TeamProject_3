5d60200de258a5abcf903e6ff4b82dbd68ce0cab
namsuhan0322 <rnatpakfl@gmail.com>
2024-06-17 09:44:16 +0900

    [Feat] 슬라이더값 로컬 값에 저장

--- 3Match_Puzzle_Game/Assets/Scripts/Audio/AudioMixerController.cs
+++ 3Match_Puzzle_Game/Assets/Scripts/Audio/AudioMixerController.cs
@@ -25,18 +25,29 @@ public class AudioMixerController : MonoBehaviour
         musicSFXSlider.onValueChanged.AddListener(SetSFXVolume);               
     }
 
+    void Start()
+    {
+        musicMasterSlider.value = PlayerPrefs.GetFloat("Master");
+        musicBGMSlider.value = PlayerPrefs.GetFloat("BGM");
+        musicSFXSlider.value = PlayerPrefs.GetFloat("SFX");
+
+    }
+
     public void SetMasterVolume(float volume)                           // 마스터 볼륨 슬라이더가 Mixer에 반영되게
     {
         audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);        // 볼륨은 Log10 단위에 x20을 해준다.
+        PlayerPrefs.SetFloat("Master", volume);
     }
 
     public void SetBGMVolume(float volume)                              // BGM 볼륨 슬라이더가 Mixer에 반영되게
     {
         audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("BGM", volume);
     }
 
     public void SetSFXVolume(float volume)                              // SFX 볼륨 슬라이더가 Mixer에 반영되게
     {
         audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFX", volume);
     }
 }
